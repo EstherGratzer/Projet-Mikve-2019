ff272cef890425a4519f13e8c99d95414667a79b
Serenity2812 <sadjedj@gmail.com>
2019-01-13 15:32:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+
+require_once ("Model/Manager.php");
+require_once ("Model/User.php");
+
+$firstname = "shalva";
+$lastname = "adjedj";
+$login = "shalva";
+$password = "serenite";
+$profil_pic = "";
+$rights = "";
+
+
+$user = new user();
+$user->addUser($firstname, $lastname, $login, $password, $profil_pic, $rights);
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,7 +22,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Mikve</title>
 </head>
 <body>
 
