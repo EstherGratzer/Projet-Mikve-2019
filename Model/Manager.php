911c2763763d3273a6c36643d6dc91c527bd0be6
Serenity2812 <sadjedj@gmail.com>
2019-01-15 09:04:13 +0200

    Update Manager.php

--- Model/Manager.php
+++ Model/Manager.php
@@ -4,7 +4,10 @@ class Manager {
 
     protected function dbConnect()
     {
-        $db = new PDO('mysql:host=localhost;dbname=projetmikve; charset=utf8', 'root', '');
+        $ini = parse_ini_file('config.ini');
+        echo $ini['db_name'];     // mydatabase
+        echo $ini['db_user'];
+        $db = new PDO('mysql:host='.$ini['db_host'].';dbname='.$ini['db_name'].'; charset=utf8', $ini['db_user'], $ini['db_password']);
         return $db;
     }
 }
