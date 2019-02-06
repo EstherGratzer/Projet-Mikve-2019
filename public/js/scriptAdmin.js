edc29d53e2841af04d7cdf53e0fe71ec524acd09
anaelleamar <anaelleh19@gmail.com>
2019-02-06 12:05:35 +0200

    page admin que je n'ai pas (du tt) fini

--- public/js/scriptAdmin.js
+++ public/js/scriptAdmin.js
@@ -23,42 +23,61 @@ $(function()
             });
         }
     });
-
     $(document).on("click","ul.navbar-nav li a", function(e)
     {
         var action = $(this).data("action");
         $.ajax({
             url: "admin.php?action="+action,
             type: "POST",
-
-            success : function(resultDataList){
+            success : function(resultDataList)
+            {
                 $(".listContent").removeClass('hidden').html(resultDataList);
             },
-
-            error : function () {
+            error : function ()
+            {
                 alert('Une erreur s\'est produite');
             }
 
         });
     });
-
-     $(document).on('click', 'span.delete-halaha', function(){
+    // delete d'une halakha
+     $(document).on('click', 'span.delete-halaha', function()
+     {
          var halahaToDelete = $(this).data('halaha');
-         if(confirm("Etes vous sur de supprimer?"))
+         if(confirm("Etes vous sûr de vouloir supprimer cette Halakha ?"))
          {
-             $.ajax({
+             $.ajax(
+             {
                  url:"admin.php?action=deleteHalaha",
                  method:"POST",
                  data:{id: halahaToDelete.id},
-
-                 success:function(data){
-                     $('#alert_message').html('<div class="alert alert-success">La Halaha "' +  halahaToDelete.titre +'" a ete supprimée!</div>');
+                 success:function(data)
+                 {
+                     $('#alert_message').html('<div class="alert alert-success">La Halaha "' +  halahaToDelete.titre +'" a ete supprimée !</div>');
                      $('.halaha-'+halahaToDelete.id).fadeOut();
                  }
              });
          }
      });
-
+     // delete d'un mikvé
+    $(document).on('click', 'span.delete-mikve', function()
+    {
+        var mikveToDelete = $(this).data('mikve');
+        if(confirm("Etes vous sûr de vouloir supprimer ce mikvé ?"))
+        {
+            $.ajax(
+            {
+                url:"admin.php?action=deleteMikve",
+                method:"POST",
+                data:{mikves_id: mikveToDelete.id},
+                success:function(data)
+                {
+                    $('#alert_message').html('<div class="alert alert-success">Le Mikvé "' +  mikveToDelete.name +'" a ete supprimé !</div>');
+                    $('.mikve-'+mikveToDelete.id).fadeOut();
+                }
+            });
+        }
+    });
     $("form.newEdit").submit(function(e) {
         e.preventDefault();
         var formData = new FormData($(this)[0]);
@@ -75,26 +94,53 @@ $(function()
         });
         return false;
     });
-
-
-    $(document).on('click', 'span.edit', function(){
+    $(document).on('click', 'span.edit', function()
+    {
         var elementIdToShow = $(this).data("href");
-        if ($('#'+elementIdToShow).hasClass('hidden')){
-            $.ajax({
+        if ($('#'+elementIdToShow).hasClass('hidden'))
+        {
+            $.ajax(
+            {
                 url:"admin.php?action=edit",
                 method:"GET",
-                data:{id: $(this).data('id'), type : $('#list').data('type')},
-
-                success:function(data){
+                data:
+                {
+                    id: $(this).data('id'),
+                    type : $('#list').data('type')
+                },
+                success:function(data)
+                {
                     console.log(data);
                     $('#' + elementIdToShow).find('td').html(data);
                 }
             });
         }
         $('#'+elementIdToShow).toggleClass('hidden');
-
     });
-
+    // update d'un mikvé
+    $(document).on('click', 'span.edit', function()
+    {
+        var elementIdToShow = $(this).data("href");
+        if ($('#'+elementIdToShow).hasClass('hidden'))
+        {
+            $.ajax(
+            {
+                url:"admin.php?action=edit",
+                method:"GET",
+                data:
+                {
+                    id: $(this).data('id'),
+                    type : $('#list').data('type')
+                },
+                success:function(data)
+                {
+                    console.log(data);
+                    $('#' + elementIdToShow).find('td').html(data);
+                }
+            });
+        }
+        $('#'+elementIdToShow).toggleClass('hidden');
+    });
 });
 
 
